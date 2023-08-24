f0be062f722f6b6f92b128a94b1074f4ac233ca1
rakesh-i <rakesh.007.ac@gmail.com>
2023-08-24 15:44:49 +0530

    ui update and added shadiing

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const content = document.getElementById('container');
 content.style.height = '550px';
 content.style.width = '550px';
-// const clear = document.getElementById('clear');
+const rbut = document.querySelector('#rainbow');
+const sbut = document.querySelector('#shading');
 var rain = 0;
-
+var shading = 0;
 
 function creategrid(side){
     content.style.gridTemplateColumns = `repeat(${side},1fr)`;
@@ -14,13 +15,13 @@ function creategrid(side){
         gridcell.classList.add("gridCell");
         content.append(gridcell);
     }
+    clearboard();
     draw();
 }
 
 function clearboard(){
-    console.log("hello");
     let grid = document.querySelectorAll('.gridCell');
-    grid.forEach((div) => (div.style.backgroundColor='white'));
+    grid.forEach((div) => (div.style.backgroundColor='rgb(255,255,255)'));
 }
 
 function select(){
@@ -30,14 +31,45 @@ function select(){
 
 function rainbow(){
     rain = (rain==1)?0:1;
+    
+    if(rain==1){
+        rbut.style.background = "red";
+        sbut.style.background = "hsl(0 0% 93%)";
+    }
+    else{
+        rbut.style.background = "hsl(0 0% 93%)";
+    }
+    shading = 0;
     console.log(rain);
+}
+
+function shade(){
+    shading = (shading==1)?0:1;
+    if(shading==1){
+        sbut.style.background = "red";
+        rbut.style.background = "hsl(0 0% 93%)";
+    }
+    else{
+        sbut.style.background = "hsl(0 0% 93%)";
+    }
+    rain = 0;
+    console.log(shading);
 }
 
 function draw(){
     let grid = document.querySelectorAll('.gridCell');
     grid.forEach((div)=>div.addEventListener('mouseover',(e)=>{
+        
         if(rain===1){
             e.target.style.backgroundColor = `hsl(${Math.floor(Math.random() * 361)}, 100%, 50%)`;
+        }
+        else if(shading==1){
+            let x = e.target.style.backgroundColor;
+            x = x.slice(4,-1);
+            let y = x.split(",");
+            if(y[0]>0){
+                e.target.style.backgroundColor = `rgb(${y[0]-25},${y[0]-25},${y[0]-25})`;
+            }
         }
         else{
             e.target.style.backgroundColor = 'black';
@@ -47,6 +79,4 @@ function draw(){
 }
 
 creategrid(5);
-
-// content.append(grid);
 
